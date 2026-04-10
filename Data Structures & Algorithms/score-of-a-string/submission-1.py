81ece73afa142eef2729b6fe0c1cc9598e67baf0
Aaron Zambrano <47159699+aazambrano2@users.noreply.github.com>
2026-04-10 12:17:19 -0500

    Add: score-of-a-string - submission-1

---
+++ Data Structures & Algorithms/score-of-a-string/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        '''
+        Input: s = "code"
+
+        Output: 24
+        Explanation: The ASCII values of the characters in the given string are: 'c' = 99, 'o' = 111, 'd' = 100, and 'e' = 101. The score of s will be: |111 - 99| + |100 - 111| + |101 - 100|.
+        
+        '''
+        if len(s) == 2:
+            return abs(ord(s[1])-ord(s[0]))
+        sum = 0
+        for i in range(len(s)-1):
+            sum += abs(ord(s[i+1])-ord(s[i]))
+        return sum
+        #Time: O(N)
+        #Space: O(1)
+
+
+        
